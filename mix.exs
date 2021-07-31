679530ed3743603af741e46e2248914b03dfd644
Owen Bickford <historicstork@gmail.com>
2021-07-31 00:38:58 +0000

    add pow and pow_assent

--- mix.exs
+++ mix.exs
@@ -46,6 +46,8 @@ defmodule OwenCode.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:pow, "~> 1.0"},
+      {:pow_assent, "~> 0.4.11"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
