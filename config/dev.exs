bb45ec1fbe4ab2da7227c8a8b34e64f03aa757a6
Owen Bickford <owen@owencode.com>
2021-08-07 23:51:09 -0400

    update pgsql dev password

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :owen_code, OwenCode.Repo,
   username: "owen",
-  password: "",
+  password: "owen",
   database: "owen_code_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
