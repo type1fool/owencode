0f17f149c2a491f13de2f471a37b15fdb20b2353
Owen Bickford <owen+github@owencode.com>
2021-08-28 17:24:38 -0400

    fix credo warnings

--- lib/owencode/mailer.ex
+++ lib/owencode/mailer.ex
@@ -1,3 +1,4 @@
 defmodule OwenCode.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :owencode
 end
--- lib/owencode_web/telemetry.ex
+++ lib/owencode_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule OwenCodeWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
