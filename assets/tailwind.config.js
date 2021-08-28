34d7df91fe0ba065c615d3b0c0d40fd8cb84408d
Owen Bickford <owen+github@owencode.com>
2021-08-28 16:39:54 -0400

    add postcss and tailwind config

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
