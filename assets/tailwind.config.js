aa49c60016becdbda9f7434ba0ae772c00b8e63d
Owen Bickford <owen+github@owencode.com>
2021-09-05 17:30:18 -0400

    set color aliases

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,9 +1,20 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   mode: "jit",
   purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: colors.blueGray,
+        brand: colors.violet,
+        success: colors.lime,
+        warning: colors.amber,
+        danger: colors.rose,
+        info: colors.sky,
+      },
+    },
   },
   variants: {
     extend: {},
