0d0c39a8cf9285852d8edcb573516463b9e718c5
Owen Bickford <owen+github@owencode.com>
2021-09-06 16:48:24 -0400

    set browser title to post title

--- lib/owencode_web/live/post_live/show.ex
+++ lib/owencode_web/live/post_live/show.ex
@@ -11,10 +11,12 @@ defmodule OwenCodeWeb.PostLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    post = Blog.get_post_by_id!(id)
+
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:post, Blog.get_post_by_id!(id))}
+     |> assign(:page_title, post.title)
+     |> assign(:post, post)}
   end
 
   defp page_title(:show), do: "Show Post"
