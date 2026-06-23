356145022c263753cb6c00564d6a35f8af201f4f
Owen Bickford <owen@owencode.com>
2026-06-23 13:10:50 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Owencode.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.2"},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"}
